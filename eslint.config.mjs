770109f6d6585cc2cb0efdb2c3f7ad63c5dfee26
mimifuwa <mimifuwa.cc@gmail.com>
2025-06-23 01:01:17 +0900

    chore: add dev dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,78 @@
+// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: eslint.configs.recommended,
+  allConfigs: eslint.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default tseslint.config(
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "build/",
+      "out/",
+      "public/",
+      ".next/",
+      "**/*.min.js",
+      "lefthook.yml",
+      "package-lock.yaml",
+    ],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  {
+    plugins: {
+      import: importPlugin,
+    },
+    rules: {
+      "sort-imports": ["error", { ignoreCase: true, ignoreDeclarationSort: true }],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", ["sibling", "parent"], "object"],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+          ],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "react/display-name": ["error", { ignoreTranspilerName: false }],
+      "array-callback-return": "error",
+      "no-await-in-loop": "error",
+      "no-constructor-return": "error",
+      "no-duplicate-imports": "error",
+      "no-inner-declarations": "error",
+      "no-promise-executor-return": "error",
+      "no-self-compare": "error",
+      "no-template-curly-in-string": "warn",
+      "no-unmodified-loop-condition": "error",
+      "no-unreachable-loop": "error",
+      "no-use-before-define": "error",
+      "no-useless-assignment": "error",
+      "require-atomic-updates": "error",
 
-export default eslintConfig;
+      "@next/next/no-img-element": "off",
+    },
+  }
+);
